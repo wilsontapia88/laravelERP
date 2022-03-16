3b4df4648bf99a74e1fe9c9f59887defe7f2fdc7
Wilson Tapia <wilsontapia88@gmail.com>
2022-03-16 10:06:37 -0300

    regras do nogocio no controller

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 use App\Models\Order;
 use App\Jobs\PostServers;
 
+
 class OrderController extends Controller
 {
     private $order;
@@ -22,8 +23,57 @@ class OrderController extends Controller
 
         $dados = collect($request->all());
         $groupOrders = $dados->groupBy('ArticleCode');
-        $modelOrders = $this->order->groupOrders($groupOrders);
-        return response()->json($modelOrders);
+
+        $valTotalOrder = 0;
+        foreach($groupOrders as $index => $groupOrder){
+
+            $qtdtotal = 0;
+            foreach($groupOrder as $order){
+                $qtdtotal += $order['Quantity'];
+                $datos = [
+                    "ArticleCode" => $order['ArticleCode'],
+                    "ArticleName" => $order['ArticleName'],
+                    "UnitPrice"   => $order['UnitPrice'],
+                    "Quantity"    => $qtdtotal
+                ];
+            }
+
+            $valorTotalArticle = $order['UnitPrice'] * $qtdtotal;
+
+            $valTotalOrder += $valorTotalArticle;
+
+            if($qtdtotal >= 5 && $qtdtotal <= 9 && $valTotalOrder >= 500){
+                $valor_descontado = $valTotalOrder - $valTotalOrder * 30 / 100.0;
+            }else{
+                $valor_descontado = 0;
+            }
+            $groupOrders[$index] = $datos;
+        }
+
+        $dadosRetorno = [   
+            'OrderId'                       => null,
+            'OrderCode'                     => null,                                     
+            'OrderDate'                     => date('Y-m-d'), # data do pedido no formato YYYY-MM-DD
+            'TotalAmountWihtoutDiscount'    => $valTotalOrder,  # preço total sem desconto
+            'TotalAmountWithDiscount'       => $valor_descontado,   # preço total com desconto
+            'OrderInfo'                     => $groupOrders
+        ];
+ 
+        $nOrder = Order::create($dadosRetorno);
+        $nOrder->OrderCode = date('Y-m-').$nOrder->id; # código no formato YYYY-MM-OrderId
+        $nOrder->save();
+
+        unset($dadosRetorno['OrderInfo']);
         
+        $dadosRetorno['OrderId'] = $nOrder->id;
+        $dadosRetorno['OrderCode'] = date('Y-m-').$nOrder->id;
+
+        //job para teste o job funciona como funcao
+        // dispatch_now(new PostServers($dadosRetorno));
+
+        //job para encaminhar a utilizacao a todos os server
+        dispatch(new PostServers($dadosRetorno));
+
+        return response()->json($dadosRetorno);        
     }
 }
